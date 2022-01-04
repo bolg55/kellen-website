52e9242a516e6e2f574e83f6d2abd7ef5ce680ca
bolg55 <bolger.kellen@gmail.com>
2022-01-03 23:39:42 -0500

    added h1

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -25,10 +25,10 @@ const Sidebar = () => {
         width='128'
         quality='100'
       />
-      <h3 className='my-4 text-3xl font-medium tracking-wider font-kaushan'>
+      <h1 className='my-4 text-3xl font-medium tracking-wider font-kaushan'>
         <span className='text-green'>Kellen </span>
         Bolger
-      </h3>
+      </h1>
       <p className='px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200'>
         Web Developer
       </p>
